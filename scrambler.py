69f3e338e27ca3d430c87ce2e176f2e28574b3e7
O-Towolawi <setowolawi3@gmail.com>
2023-03-10 21:38:51 +0000

    added repeats
    user can ask for as many scrambles as they want before returning to menu

--- scrambler.py
+++ scrambler.py
@@ -6,6 +6,21 @@ moves = {1: all_moves[:6], 2: all_moves[:6], 3: all_moves[:8], 4: all_moves}
 mdir = ["", "'", "w"] #move direction
 mcount = ["", "2"] #move count
 
+def details():
+  csize = int(input("Enter cube size [1-4]: ")) #cube size
+  try:
+      slen = int(input("Scramble length: ")) #length of scramble
+      while slen < 1:
+          print("Please enter a positive integer.")
+          slen = int(input("Scramble length: "))
+  except:
+      print("Please enter a positive integer.")
+      slen = int(input("Scramble length: "))
+      while slen < 1:
+          print("Please enter a positive integer.")
+          slen = int(input("Scramble length: "))
+  return (csize, slen)
+
 def not_dict(): #notation dictionary
   notation = {"L": "downwards", "R": "upwards", "U": "towards your left hand", "D": "towards your right hand", "F": "clockwise", "B": "anti-clockwise", "M": "downwards", "x": "away from you", "l": "inner left column (second from the right). Moves downwards", "r": "inner right column (third from the right). Moves upwards", "u": "inner upper row (second from the top). Moves towards your left hand", "d": "inner bottom row (third from the top). Moves towards your right hand", "f": "second from the front. Moves clockwise.", "b": "third from the front. Moves anti-clockwise."}
   special_notation = {"' (i.e. R')": "moves in opposite direction to usual. E.g. R' moves downwards", "w (i.e. Rw)": "moves itself and it's lowercase notation. E.g. Rw means moving R and r (same as saying Rr, often called R sqaured)", "2 (i.e. R2)": "Repeat the notation twice. E.g. R2 means move the rightmost column upwards twice consecutively."}
@@ -25,20 +40,7 @@ def not_dict(): #notation dictionary
   print()
   menu()
 
-def gen_scramble():
-  csize = int(input("Enter cube size [1-4]: ")) #cube size
-  try:
-      slen = int(input("Scramble length: ")) #length of scramble
-      while slen < 1:
-          print("Please enter a positive integer.")
-          slen = int(input("Scramble length: "))
-  except:
-      print("Please enter a positive integer.")
-      slen = int(input("Scramble length: "))
-      while slen < 1:
-          print("Please enter a positive integer.")
-          slen = int(input("Scramble length: "))
-  print()
+def gen_scramble(csize, slen):
   print((str(csize) + "x")*2 + str(csize) + " Scramble" + " - " + str(slen) + " steps" )
   s = valid([[random.choice(moves[csize]), random.choice(mdir), random.choice(mcount)] for i in range(slen)], csize)
   return ''.join(s[x][0] + s[x][1] + s[x][2] + " "for x in range(len(s)))
@@ -73,20 +75,27 @@ def menu():
   
   How can we help you today [1-4]? """))
   print()
-  if option == 1:
+  if option == 1: #notation dictionary
      print(not_dict())
-  elif option == 2:
-     print(gen_scramble())
-     print()
-     repeat = input("Would you like another scramble (y/n)? ")
-     while repeat == "y" or repeat == "yes":
-        print(gen_scramble())
-        print()
-        repeat = input("Would you like another scramble (y/n)? ")
-     print()
-     print("Very well. Returning to menu...")
-     print()
-     menu()
+  elif option == 2: #scrambler
+    csize, slen = details()
+    print(gen_scramble(csize, slen))
+    print()
+    repeat = input("Would you like another scramble (y/n)? ")
+    while repeat == "y" or repeat == "yes":
+      same = input("Same details (y/n)? ")
+      print()
+      if same == "y" or same == "yes":
+        print(gen_scramble(csize, slen))
+      else:
+        csize, slen = details()
+        print(gen_scramble(csize, slen))
+      print()
+      repeat = input("Would you like another scramble (y/n)? ")
+    print()
+    print("Very well. Returning to menu...")
+    print()
+    menu()
   elif option == 3:
      print(pattern_dict())
   elif option == 4:
